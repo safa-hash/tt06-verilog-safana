b80ff610b4e242dbecf68e02aeb9b0359148a726
Safana Rafeeque <83155341+safa-hash@users.noreply.github.com>
2024-04-30 12:43:23 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_safana (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,10 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[0] =  ui_in[0] & ~ui_in[4] & ~ui_in[5] | ui_in[1] & ~ui_in[4] & ui_in[5] | ui_in[2] & ~ui_in[4] & ui_in[5] | ui_in[3] & ~ui_in[4] & ui_in[5];
+        
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+    assign uo_out[7:1] = 7*b0;
 endmodule
